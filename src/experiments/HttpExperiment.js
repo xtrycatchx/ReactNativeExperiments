6390f23bfd0b0ce4f0eaac8c5e2d71f1178a6b9b
Paul Sydney <xtrycatchx@gmail.com>
2018-02-28 21:47:16 +0800

    http experiments

--- src/experiments/HttpExperiment.js
+++ src/experiments/HttpExperiment.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Button, View, } from 'react-native';
+import { Button, View, AlertIOS} from 'react-native';
+
+const END_POINT = 'https://jsonplaceholder.typicode.com'
 
 class HttpExperiment extends Component {
 
@@ -8,7 +10,19 @@ class HttpExperiment extends Component {
     }
 
     onPressPostButton = () => {
-        //
+        fetch(`${END_POINT}/posts`, {
+            method: 'POST',
+            body: JSON.stringify({
+                title: 'foo',
+                body: 'bar',
+                userId: 1
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(response => response.json())
+            .then(json => AlertIOS.alert(JSON.stringify(json)))
     };
 
     render() {
@@ -18,7 +32,7 @@ class HttpExperiment extends Component {
 
                 <Button
                     onPress={this.onPressPostButton}
-                    title='Http stuff'
+                    title='Http Post'
                 />
 
             </View>
