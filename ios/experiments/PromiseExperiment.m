26cfc33086fded5fe251e8db62e066d2eef7afb4
Paul Sydney <xtrycatchx@gmail.com>
2018-02-28 11:36:50 +0800

    added xcode part for the promise

--- ios/experiments/PromiseExperiment.m
+++ ios/experiments/PromiseExperiment.m
@@ -13,4 +13,36 @@
 // export the module
 RCT_EXPORT_MODULE();
 
+RCT_EXPORT_METHOD(promiseWillResolve:
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+  RCTLogInfo(@"INSIDE TEST promiseWillResolve");
+  resolve(nil);
+}
+
+RCT_EXPORT_METHOD(promiseWillResolveWithParams: (NSString *)params
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+  RCTLogInfo(@"INSIDE TEST promiseWillResolveWithParams");
+  NSArray *array = [NSArray arrayWithObjects:params, nil];
+  resolve(array);
+}
+
+RCT_EXPORT_METHOD(promiseWillReject:
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+  RCTLogInfo(@"INSIDE TEST promiseWillResolve");
+  NSError *error = [NSError errorWithDomain:@"some_domain" code:100 userInfo:@{ NSLocalizedDescriptionKey:@"Something went wrong" }];
+  reject(@"probs", @"Expected error in xcode", error);
+}
+
+/**onPressPromiseWillResolveButton = () => this.executePromise(PromiseExperiment.promiseWillResolve())
+
+onPressPromiseWillResolveWithParamsButton = () => this.executePromise(PromiseExperiment.promiseWillResolveWithParams(new Date().toString()));
+
+onPressPromiseWillRejectButton = () => this.executePromise(PromiseExperiment.promiseWillReject())**/
+
 @end
