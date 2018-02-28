9786cc91e39a770fc80ece596edf7222b187218a
Paul Sydney <xtrycatchx@gmail.com>
2018-02-28 11:09:04 +0800

    added xcode part for the promise

---
+++ ios/experiments/PromiseExperiment.h
@@ -0,0 +1,11 @@
+//
+//  PromiseExperiment.h
+//  ReactNativeExperiments
+//
+//  Created by Paul Sydney on 28/2/18.
+//
+
+#import <React/RCTBridgeModule.h>
+
+@interface PromiseExperiment : NSObject <RCTBridgeModule>
+@end
---
+++ ios/experiments/PromiseExperiment.m
@@ -0,0 +1,16 @@
+//
+//  PromiseExperiment.m
+//  ReactNativeExperiments
+//
+//  Created by Paul Sydney on 28/2/18.
+//
+
+#import "PromiseExperiment.h"
+#import <React/RCTLog.h>
+
+@implementation PromiseExperiment
+
+// export the module
+RCT_EXPORT_MODULE();
+
+@end
