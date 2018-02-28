186c4cb737cbb3e16c0ada02ac3cf74ae39b50a9
Paul Sydney <xtrycatchx@gmail.com>
2018-02-28 11:37:35 +0800

    RN part to invoke xcode layer

--- src/experiments/PromiseExperiment.js
+++ src/experiments/PromiseExperiment.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, NativeModules, AlertIOS } from 'react-native';
+
+const promiseExperiment = NativeModules.PromiseExperiment;
 
 class PromiseExperiment extends Component {
 
@@ -7,12 +9,39 @@ class PromiseExperiment extends Component {
         super(props);
     }
 
+    executePromise = promise => {
+        promise.then(data => {
+            data ? AlertIOS.alert(`Success ${data}`) : AlertIOS.alert(`Success`)
+        }).catch(err => {
+            AlertIOS.alert(`${err}`)
+        })
+    }
+
+    onPressPromiseWillResolveButton = () => this.executePromise(promiseExperiment.promiseWillResolve({}))
+
+    onPressPromiseWillResolveWithParamsButton = () => this.executePromise(promiseExperiment.promiseWillResolveWithParams(new Date().toString()));
+
+    onPressPromiseWillRejectButton = () => this.executePromise(promiseExperiment.promiseWillReject({}))
+
     render() {
 
         return (
             <View style={{ flex: 1, padding: 10 }}>
 
-                <Text>Promise Experiment</Text>
+                <Button
+                    onPress={this.onPressPromiseWillResolveButton}
+                    title='XCode will Resolve'
+                />
+
+                <Button
+                    onPress={this.onPressPromiseWillResolveWithParamsButton}
+                    title='XCode will Resolve + params'
+                />
+
+                <Button
+                    onPress={this.onPressPromiseWillRejectButton}
+                    title='XCode will Reject + params'
+                />
 
             </View>
         );
