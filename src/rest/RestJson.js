023073c8be178bfd9ed9d8330a173af272f35637
Paul Sydney <xtrycatchx@gmail.com>
2018-03-01 09:50:40 +0800

    cleanup

--- src/rest/RestJson.js
+++ src/rest/RestJson.js
@@ -1,30 +1,31 @@
 'use strict';
 
-function doCall(url, body) {
-    let opt = {};
-    opt.body = JSON.stringify(body) || null
-    //opt.headers['X-API-KEY'] = 'unya-na';
-    opt.method = body ? 'POST' : 'GET'
-    opt.headers = {
-        "Content-type": "application/json; charset=UTF-8"
+const doCall = (url, body) => {
+    const opt = {
+        body: JSON.stringify(body) || null,
+        method: body ? 'POST' : 'GET',
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
     }
     return fetch(url, opt).then(doResponse, doNetworkError);
 }
 
-function doResponse(response) {
+const doResponse = (response) => {
     if (response.ok) {
         return response.json();
     } else {
-        return response.json().then((error) => {
+        return response.json().then(error => {
             throw error;
         });
     }
 }
 
-function doNetworkError(error) {
+const doNetworkError = error => {
     throw {
         msg: error.message
     };
 }
-module.exports.get = doCall;
-module.exports.post = doCall;
+
+exports.get = doCall;
+exports.post = doCall;
