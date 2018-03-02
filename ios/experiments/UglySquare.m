4119a83d434a4ab97712daee48e0d9de8a48d66c
Paul Sydney <xtrycatchx@gmail.com>
2018-03-02 11:57:20 +0800

    bridge objective-c UI to RN

--- ios/experiments/UglySquare.m
+++ ios/experiments/UglySquare.m
@@ -3,7 +3,19 @@
 //  ReactNativeExperiments
 //
 //  Created by Paul Sydney on 2/3/18.
-//  Copyright © 2018 Facebook. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@class RCTEventDispatcher;
+
+@interface UglySquare : UIView
+
+  // view property with @property
+  @property(nonatomic, assign) NSString *exampleProp;
+
+  // init event dispatcher that allows us to talk communicate with JS
+- (instancetype) initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher NS_DESIGNATED_INITIALIZER;
+
+@end
+
