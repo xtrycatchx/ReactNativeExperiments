40f65d6df8ec027e68504aa3dd06c7ef6a9bbb3c
Paul Sydney <xtrycatchx@gmail.com>
2018-03-04 15:11:55 +0800

    experimenting apple push notification

---
+++ src/experiments/PushNotificationExperiment.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Picker, AppState, Platform } from 'react-native';
+import PushHandler from '../push/PushHandler';
+import PushNotification from 'react-native-push-notification';
+
+class PushNotificationExperiment extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleAppStateChange = this.handleAppStateChange.bind(this);
+        this.state = {
+            seconds: 5,
+        };
+    }
+
+    componentDidMount() {
+        AppState.addEventListener('change', this.handleAppStateChange);
+    }
+
+    componentWillUnmount() {
+        AppState.removeEventListener('change', this.handleAppStateChange);
+    }
+
+    handleAppStateChange(appState) {
+        if (appState === 'background') {
+            let date = new Date(Date.now() + (this.state.seconds * 1000));
+
+            if (Platform.OS === 'ios') {
+                date = date.toISOString();
+            }
+
+            PushNotification.localNotificationSchedule({
+                message: "My Notification Message",
+                date,
+            });
+        }
+    }
+
+    onPressPromiseWillResolveButton = () => this.executePromise(promiseExperiment.promiseWillResolve({}))
+
+    onPressPromiseWillResolveWithParamsButton = () => this.executePromise(promiseExperiment.promiseWillResolveWithParams(new Date().toString()));
+
+    onPressPromiseWillRejectButton = () => this.executePromise(promiseExperiment.promiseWillReject({}))
+
+    render() {
+
+        return (
+            <View style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <Text>Pick Time in secs.</Text>
+                <Picker
+                    style={{ width: 100 }}
+                    selectedValue={this.state.seconds}
+                    onValueChange={(seconds) => this.setState({ seconds })}
+                >
+                    <Picker.Item label="5" value={5} />
+                    <Picker.Item label="10" value={10} />
+                    <Picker.Item label="15" value={15} />
+                </Picker>
+                <PushHandler />
+            </View>
+        );
+    }
+}
+
+export default PushNotificationExperiment;
--- src/push/PushHandler.js
+++ src/push/PushHandler.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react';
 import PushNotification from 'react-native-push-notification';
 
 export default class PushHandler extends Component {
+
+    componentDidMount() {
+        PushNotification.configure({
+          onNotification: function(notification) {
+            console.log( 'NOTIFICATION:', notification );
+          },
+        });
+      }
+
     render() {
-        return (null)
+        return null;
     }
 }
