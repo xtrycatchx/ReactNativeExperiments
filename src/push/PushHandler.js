a5ac3482dbb61a8068f9c17ab5215d147570f7c4
Paul Sydney <xtrycatchx@gmail.com>
2018-03-04 15:05:05 +0800

    experimenting apple push notification

---
+++ src/push/PushHandler.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import PushNotification from 'react-native-push-notification';
+
+export default class PushHandler extends Component {
+    render() {
+        return (null)
+    }
+}
