1b6c80696476b92727a74785fd07f4133604a5b5
Paul Sydney <xtrycatchx@gmail.com>
2018-03-05 21:42:07 +0800

    added transition from RN to Native View Controller

---
+++ ios/experiments/SampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  SampleViewController.h
+//  ReactNativeExperiments
+//
+//  Created by Paul Sydney on 5/3/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SampleViewController : UIViewController
+
+@end
---
+++ ios/experiments/SampleViewController.m
@@ -0,0 +1,37 @@
+//
+//  SampleViewController.m
+//  ReactNativeExperiments
+//
+//  Created by Paul Sydney on 5/3/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import "SampleViewController.h"
+
+@interface SampleViewController ()
+
+@end
+
+@implementation SampleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
