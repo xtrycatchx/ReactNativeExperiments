57f5808450ca658c128fea7f2b62158983cb9ea9
Paul Sydney <xtrycatchx@gmail.com>
2018-03-06 23:36:41 +0800

    RN open native controller

--- ios/experiments/ViewToController.m
+++ ios/experiments/ViewToController.m
@@ -3,7 +3,52 @@
 //  ReactNativeExperiments
 //
 //  Created by Paul Sydney on 6/3/18.
-//  Copyright © 2018 Facebook. All rights reserved.
 //
-
 #import <Foundation/Foundation.h>
+#import "ViewToController.h"
+
+// import RCTEventDispatcher
+#if __has_include(<React/RCTEventDispatcher.h>)
+#import <React/RCTEventDispatcher.h>
+#elif __has_include(“RCTEventDispatcher.h”)
+#import “RCTEventDispatcher.h”
+#else
+#import “React/RCTEventDispatcher.h” // Required when used as a Pod in a Swift project
+#endif
+
+
+@implementation ViewToController : UIView
+{
+  RCTEventDispatcher *_eventDispatcher;
+}
+
+-(void)didMoveToWindow
+{
+  [super didMoveToWindow];
+  if( self.window) {
+    // TODO
+    [_eventDispatcher sendAppEventWithName:@"onLoaded" body:@{@"name": @"onStop"}];
+    
+    // end of TODO
+  }
+}
+
+
+
+- (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher
+{
+  if ((self = [super init]))
+  {
+    _eventDispatcher = eventDispatcher;
+  }
+  return self;
+}
+
+- (void)setBatmanMessage:(NSString *)batmanMessage
+{
+  if(![batmanMessage isEqual:_batmanMessage])
+  {
+  }
+}
+
+@end
