644356ddda8bddbda93d602e25661393fd868ac1
Paul Sydney <xtrycatchx@gmail.com>
2018-03-06 23:40:41 +0800

    RN open native controller

---
+++ ios/experiments/ViewToControllerManager.m
@@ -0,0 +1,46 @@
+//
+//  ViewToControllerManager.m
+//  ReactNativeExperiments
+//
+//  Created by Paul Sydney on 6/3/18.
+//
+#import <Foundation/Foundation.h>
+#import "ViewToController.h"
+#import "ViewToControllerManager.h"
+
+// import RCTBridge
+#if __has_include(<React/RCTBridge.h>)
+#import <React/RCTBridge.h>
+#elif __has_include(“RCTBridge.h”)
+#import “RCTBridge.h”
+#else
+#import “React/RCTBridge.h” // Required when used as a Pod in a Swift project
+#endif
+
+@implementation ViewToControllerManager
+
+@synthesize bridge = _bridge;
+
+// Export a native module
+// https://facebook.github.io/react-native/docs/native-modules-ios.html
+RCT_EXPORT_MODULE();
+
+// Return the native view that represents your React component
+- (UIView *)view
+{
+  return [[ViewToController alloc] initWithEventDispatcher:self.bridge.eventDispatcher];
+}
+
+RCT_EXPORT_VIEW_PROPERTY(batmanMessage, NSString)
+
+// Export constants
+// https://facebook.github.io/react-native/releases/next/docs/native-modules-ios.html#exporting-constants
+- (NSDictionary *)constantsToExport
+{
+  return @{
+           @"EXAMPLE": @"example"
+           };
+}
+
+@end
+
