0e904a2a1742c7013d07832c264ab058d2834b64
Paul Sydney <xtrycatchx@gmail.com>
2018-03-07 00:03:59 +0800

    RN open native controller

--- src/native/RnOpenNativeViewController.js
+++ src/native/RnOpenNativeViewController.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import { requireNativeComponent, NativeAppEventEmitter } from 'react-native'
+import { requireNativeComponent, NativeAppEventEmitter, View, TouchableHighlight, Text } from 'react-native'
 
 const ViewToController = requireNativeComponent('ViewToController', ViewToController)
 
 export default class RnOpenNativeViewController extends Component {
     constructor(props) {
+        super(props)
         this.state = {
             nativeIsOpen: false
         }
@@ -16,23 +17,22 @@ export default class RnOpenNativeViewController extends Component {
         });
     }
     render() {
+        //return <ViewToController batmanMessage="ngeh!" style={{ height: 100, width: 100 }} {...this.props} />
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#ffffff' }}>
                 {
-                    !this.state.kyc
+                    !this.state.nativeIsOpen
                         ?
                         <ViewToController batmanMessage="ngeh!" style={{ height: 100, width: 100 }} {...this.props} />
                         :
-
                         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#ffffff' }}>
-                            <TouchableHighlight activeOpacity={0.6} underlayColor={'white'} onPress={() => this.verifyToKycServer()} >
-                                <Text style={styles.button} > Verify To KYC Server </Text>
+                            <TouchableHighlight activeOpacity={0.6} underlayColor={'white'} onPress={() =>this.props.navigation.navigate("Main", {})} >
+                                <Text> Now back in React Native </Text>
                             </TouchableHighlight>
                         </View>
                 }
             </View>
         )
-        return
     }
 }
 
