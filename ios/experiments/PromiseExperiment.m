b3c81d78d9b3c65da7a714005c1ed710adf74d89
Paul Sydney <xtrycatchx@gmail.com>
2018-03-07 23:54:50 +0800

    clean up

--- ios/experiments/PromiseExperiment.m
+++ ios/experiments/PromiseExperiment.m
@@ -10,7 +10,6 @@
 
 @implementation PromiseExperiment
 
-// export the module
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(promiseWillResolve:
@@ -38,11 +37,5 @@ RCT_EXPORT_METHOD(promiseWillReject:
   NSError *error = [NSError errorWithDomain:@"some_domain" code:100 userInfo:@{ NSLocalizedDescriptionKey:@"Something went wrong" }];
   reject(@"probs", @"Expected error in xcode", error);
 }
-
-/**onPressPromiseWillResolveButton = () => this.executePromise(PromiseExperiment.promiseWillResolve())
-
-onPressPromiseWillResolveWithParamsButton = () => this.executePromise(PromiseExperiment.promiseWillResolveWithParams(new Date().toString()));
-
-onPressPromiseWillRejectButton = () => this.executePromise(PromiseExperiment.promiseWillReject())**/
 
 @end
