225cf3110037783876b8eb6e2f523b79d94aa255
Paul Sydney <xtrycatchx@gmail.com>
2018-04-02 18:08:45 +0800

    small cleanup

--- src/rest/RestJson.js
+++ src/rest/RestJson.js
@@ -1,25 +1,19 @@
 'use strict';
 
-const doCall = (url, body) => {
-    const opt = {
+const doCall = (url, body) =>
+    fetch(url, {
         body: JSON.stringify(body) || null,
         method: body ? 'POST' : 'GET',
         headers: {
-            "Content-type": "application/json; charset=UTF-8"
+            'Content-type': 'application/json; charset=UTF-8'
         }
-    }
-    return fetch(url, opt).then(doResponse, doNetworkError);
-}
+    }).then(doResponse, doNetworkError);
 
-const doResponse = (response) => {
-    if (response.ok) {
-        return response.json();
-    } else {
-        return response.json().then(error => {
-            throw error;
-        });
-    }
-}
+
+const doResponse = response =>
+    response.ok ? response.json() : response.json().then(error => {
+        throw error;
+    });
 
 const doNetworkError = error => {
     throw {
