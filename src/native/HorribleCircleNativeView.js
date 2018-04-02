fa4ee74c5a754026aea7c033bcdb24079391cadd
Paul Sydney <xtrycatchx@gmail.com>
2018-04-02 18:04:45 +0800

    small cleanup

--- src/native/HorribleCircleNativeView.js
+++ src/native/HorribleCircleNativeView.js
@@ -5,9 +5,7 @@ import { requireNativeComponent } from 'react-native'
 const HorribleCircle = requireNativeComponent('HorribleCircle', HorribleCircle)
 
 export default class HorribleCircleView extends Component {
-  render() {
-    return <HorribleCircle batmanMessage="ngeh!" style={{ height: 100, width: 100 }} {...this.props} />
-  }
+  render = () => <HorribleCircle batmanMessage="ngeh!" style={{ height: 100, width: 100 }} {...this.props} />
 }
 
 HorribleCircle.propTypes = {
